f677a16eb3f2e1d71b64c409606d04c71931cdd5
Жансеит Кенжебаев <k.zhanseit.2007@gmail.com>
2024-11-25 10:25:26 +0500

    feat: add form

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,27 @@
 </script>
 
 <template>
-<button type="button" class="btn btn-success">Success</button>
+<div class="container">
+    <div class="mb-3">
+  <label for="name" class="form-label">Название</label>
+  <input type="text" class="form-control" id="name">
+</div>
+<div class="mb-3">
+  <label for="date" class="form-label">Дата</label>
+  <input type="date" class="form-control" id="date">
+</div>
+<div class="mb-3">
+  <label for="count" class="form-label">Количество</label>
+  <input type="number" class="form-control" id="count">
+</div>
+<div class="mb-3">
+  <label for="price" class="form-label">Цена</label>
+  <input type="number" class="form-control" id="price">
+</div>
+<div class="button">
+    <button type="button" class="btn btn-success">Добавить</button>
+</div>
+</div>
 </template>
 
 <style scoped>
